ce2d5af83a15aed23c74b30d8a3b9ab14559fd0c
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-09-03 02:26:23 +0530

    edit

--- js-folder/login.js
+++ js-folder/login.js
@@ -1,9 +1,29 @@
-let reg_btn = document.getElementById("register_btn")
+let reg1_btn = document.getElementById("register_btn")
+let reg2_btn = document.getElementById("register_btn2")
 let form1 = document.getElementById("login_form")
 let form2 = document.getElementById("reg_form")
-let text = document.getElementById("reg_text")
-reg_btn.addEventListener("click", ()=>{
+let reg_text = document.getElementById("reg_text")
+let log_text = document.getElementById("log_text")
+let input = document.getElementsByClassName("input1")
+let log_btn = document.getElementById("login_btn")
+
+reg1_btn.addEventListener("click", () => {
     form1.classList.toggle("toggle")
     form2.classList.toggle("toggle")
-    text.classList.toggle("shift")
+    reg_text.classList.toggle("shift1")
+    log_text.classList.toggle("shift2")
+})
+reg2_btn.addEventListener("click", () => {
+    form1.classList.toggle("toggle")
+    form2.classList.toggle("toggle")
+    reg_text.classList.toggle("shift1")
+    log_text.classList.toggle("shift2")
+})
+
+
+
+log_btn.addEventListener("click", () => {
+    if (input[0].value == "souvik" && input[1].value == "111") {
+        window.location = "/html-folder/govt.html"
+    }
 })
