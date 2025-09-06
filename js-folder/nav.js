96abcef4c0526044ca824695b8969e9cbcb03f34
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-09-07 02:23:47 +0530

    editing

--- js-folder/nav.js
+++ js-folder/nav.js
@@ -12,3 +12,7 @@ function hideMenu(){
 navMenu.addEventListener('click',showMenu);
 navCross.addEventListener('click',hideMenu);
 
+// let header = document.getElementsByClassName("text-box")[0]
+let body1 = document.getElementById("main_body")
+// header.addEventListener("click", hideMenu)
+body1.addEventListener("click", hideMenu)
